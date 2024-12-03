05c08f0826f773cdc3d1d5dec8c65b6e6ddac683
nicof <nicofong99@gmail.com>
2024-12-03 18:54:23 -0500

    merge main edits

--- backend/main_merge.py
+++ backend/main_merge.py
@@ -1,1 +1,25 @@
 # the main file we'll be using for merge sort
+from backend import load_csv_file
+from mergesort import mergeSort
+
+'''
+file_path = "maxai-excel-to-csv-converted.csv"
+dataSet = load_csv_file(file_path)
+mergeSort(dataSet, 1)
+
+def searchEng(frag, notes):
+    fragList = []
+    for i in frag:
+        if notes.lower() in [j.lower() for j in i[2]]:
+            fragList.append(i)
+    return fragList
+
+searchInput = input("Please enter a note to find a fragrance!").strip()
+searchResults = searchEng(dataSet, searchInput)
+
+if searchResults:
+    print(f"Fragrance(s) featuring notes of '{searchInput}':")
+    for s in searchResults:
+        print(f"Brand: {s[0]}, Fragrance: {s[1]}, All Notes: {','.join(searchResults[2])}")
+    else:
+        print(f"There are no perfumes found featuring this note.") '''
