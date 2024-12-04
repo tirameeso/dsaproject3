c0036fc80b2e75e85bc90a74a663218ffc08f640
nicof <nicofong99@gmail.com>
2024-12-04 15:55:27 -0500

    path change ignore

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 
 def load_csv_file(file_path):
-    data = pd.read_csv("/Users/angielaptop/PycharmProjects/dsaproject3/maxai-excel-to-csv-converted.csv")
+    data = pd.read_csv("maxai-excel-to-csv-converted.csv")
     # dropping data that doesn't have value (3 perfume names not included)
     data.dropna(subset=["perfume"], inplace=True)
     data["notes"] = data["notes"].str.split(", ")
@@ -36,7 +36,7 @@ def searchEng(frag, notes):
 
 def main():
     # Load dataset
-    dataSet = load_csv_file("/Users/angielaptop/PycharmProjects/dsaproject3/maxai-excel-to-csv-converted.csv")
+    dataSet = load_csv_file("maxai-excel-to-csv-converted.csv")
     dataCopy = dataSet.copy()
     running = True
     initialInput = input("Welcome to Fragrance Note Finder! Please enter 'c' to continue or 'x' to exit the program.")
